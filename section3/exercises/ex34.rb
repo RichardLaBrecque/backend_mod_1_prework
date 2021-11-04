c9cba9e4f164969ccee221671bdf5bc65e963ee7
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-11-04 15:16:34 -0400

    add completed section3

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,18 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#the animal at 1 = ruby
+#the 3rd animal = peacock
+#the first (1st) animal = bear
+# the animal at 3 = kangaroo
+#the 5th animal = whale
+#animal at 2 = peacock
+#the sixth animal = platypus
+#animal at 4 = whale
+ puts "#{animals[1]}"
+ puts "#{animals[2]}"
+ puts "#{animals[0]}"
+ puts "#{animals[3]}"
+ puts "#{animals[4]}"
+ puts "#{animals[2]}"
+ puts "#{animals[5]}"
+ puts "#{animals[4]}"
